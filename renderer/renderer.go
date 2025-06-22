a8290335ca1b503b1e5bdded8172bd3a21ec0ad6
tacherasasi <tacherasasi@gmail.com>
2025-06-22 15:48:45 +0300

    minor code rafactors

--- renderer/renderer.go
+++ renderer/renderer.go
@@ -8,16 +8,16 @@ import (
 
 func Render(input string) (string, error) {
 	// Rendering Markdown with glamour
-	rendered, err := glamour.Render(input, "dark") 
+	rendered, err := glamour.Render(input, "dark")
 	if err != nil {
 		return "", err
 	}
 	return rendered, nil
 }
 
-func ReadFile(file string)(string,error){
+func ReadFile(file string) (string, error) {
 	// Reading file
-	content, err :=os.ReadFile(file)
+	content, err := os.ReadFile(file)
 	if err != nil {
 		return "", err
 	}
